05993ca6c9a75cfedda21646e13a8148aee69270
Quentin Loos <quentin.loos@dnsbelgium.be>
2019-10-15 17:58:03 +0200

    Use localhost without port for now

--- webapp/src/app/config/config.dist.js
+++ webapp/src/app/config/config.dist.js
@@ -1,3 +1,3 @@
-export const sysassetsBaseFsUrl = 'http://localhost:8080/';
+export const sysassetsBaseFsUrl = 'http://localhost/';
 export const jor1kBaseFsUrl = sysassetsBaseFsUrl + 'jor1kfs/sysroot/or1k/';
 export const jor1kWorkerUrl = 'jor1k-worker.js';
